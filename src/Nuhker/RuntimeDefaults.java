659f39be95194c82d7f72dff971eacf51a8ca191
coder037 <coder037@xyz.ee>
2014-11-27 03:56:18 +0200

    Some extra variables added to the plan.

--- src/Nuhker/RuntimeDefaults.java
+++ src/Nuhker/RuntimeDefaults.java
@@ -13,20 +13,20 @@ public class RuntimeDefaults {
 	// These are pre-defined default variables used for a standard launch
 	// Later I will add methods to set these from the CLI
 
-	int countOfCountries = 1; // need to decr 1 later
+	int countOfCountries = 1;
 	String defaultCountry = "EE"; // Which is Estonia, of course
 	int timeoutBetweenDownloadsMin = 3; // seconds
-	int timeoutWhileWorkingMax = 1800; // seconds, some unclean bailout after
-										// that
+	int timeoutWhileWorkingMax = 1800; // seconds, unclean bailout later
+	boolean verifiedLaunch = false; // not asking for author's name ;)
 	boolean interactive = false; // asking all these questions on CLI -> -y/N
-	boolean extraSources = false; // probably no time to implement extra sources
-									// like CleanMX
-	boolean outputDefinedManually = false; // so far not said anything on the
-											// command line
+	boolean extraSources = false; // probably no time to implement extra sources like CleanMX
+
+	int recursionDepth = 5; // Oh mama, it takes TIME!
 
 	// My bad habit from another language
 	long startTime = System.nanoTime(); // http://stackoverflow.com/questions/351565/system-currenttimemillis-vs-system-nanotime
 
+	boolean outputDefinedManually = false; // so far not said anything on the command line
 	String outputFilenamePrefix = "output";
 	String OutputFilenameDesiredPrefix = "txt";
 	String outputFilename = outputFilenamePrefix + "-" + startTime + "-"
