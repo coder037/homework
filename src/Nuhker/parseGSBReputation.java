79d77b39488c8783825bf65e54bf9010167e2a3b
Coder 037 <coder@xyz.ee>
2014-11-28 15:20:21 +0200

    Fully restructured the code. Methods created.

--- src/Nuhker/parseGSBReputation.java
+++ src/Nuhker/parseGSBReputation.java
@@ -43,59 +43,102 @@ import org.jsoup.select.*;
 		} 
 		
 		
-		// ToDo: method: public static String[] grabGSBReputation (String fqdnOrAsn)
+		// ToDo: method: public static String[] grabClientReputation (String fqdnOrAsn)
+
+		public static String[] grabClientReputation (String fqdnOrAsn) {
+/**
+ * @param either a shortened URL like www.ee or an ASN (number only)
+ * @return an array of string extracted from GSB page. 
+ */
+			String [] suspiciousSites = null;
+			// First determine whether the parameter is FQDN or ASN
+			// Inspiration: http://stackoverflow.com/questions/5585779/converting-string-to-int-in-java
+			int asNumber = 0;
+			boolean isURI = false;
+			String baseURL = "https://safebrowsing.google.com/safebrowsing/diagnostic?site=";
+			String url = "";
+			String as = "AS";
+			String workspace = "";
+			  int a = 0;
+			  int u = 1;
+			
+			// Decide whether the input string is ASN or URL
+			try {
+		      asNumber =  Integer.parseInt(fqdnOrAsn);
+		    } catch (NumberFormatException e) {
+		        isURI = true;
+		    }
+			  
+
+			 // Compose the final URL used for the request
+			if (isURI) {
+				System.out.println("The reputation check for an URI: "+ fqdnOrAsn);
+				url = baseURL + fqdnOrAsn;	
+			} else {
+				// isASN
+				System.out.println("The reputation check for an ASN: " + fqdnOrAsn);
+				url = baseURL + as + ":" +fqdnOrAsn;
+			}
+
+			// Actually connect to GSB
+			System.out.println("START grabbing: " + url);
+			try {  
+				  Document doc = Jsoup.connect(url).get();
+				  Elements meaningfulSections = doc.select("a"); // A HREF sections
+				 // find memes that contain meaningful FQDNs.
+				  for(Element fqdnCandidate : meaningfulSections) {
+
+					 workspace = fqdnCandidate.text();
+				     // System.out.println(workspace);
+
+				  	 // Checking each meme for nonsenselessness   
+				  	    if ( ! (isNonsense(workspace))) { // throw away these Google promo links
+					  	    if (workspace.contains(as)) {
+						 		System.out.println("Also found AS info: " + workspace);
+						 		System.out.println("        : is it our initial ASN or extra?");
+						 		a = a + 1;
+							// ToDo: the routine to exfiltrate all other ASNs except our initial one
+						 		// Is this ASN our initial ASN?
+					  	    } else {
+				  	    	System.out.println("Sub extracted a shit related URL: " + workspace);
+				  	    		u = u + 1;
+				  	    	// ToDo: We should extrafiltrate these to some global list
+				  	    } 
+				  	 }
+				}
+				  System.out.println("AS count:" + a);
+				  System.out.println("FQDN count:" + u);
+				  
+			}	
+			// Funny thing obtained from a source. Should we use it elsewhere?
+			// SRC: http://tutorials.jenkov.com/java-logging/logger.html
+			catch (IOException ex) {
+			  Logger.getLogger(parseGSBReputation.class.getName())
+			        .log(Level.SEVERE, null, ex);
+			   }
+			// SecondRoute
+			
+			return suspiciousSites;
+		}
 		
 		
 	public static void main(String[] args) {
-		String url1 = "https://safebrowsing.google.com/safebrowsing/diagnostic?site=www.ee/";
-		String url2 = "https://safebrowsing.google.com/safebrowsing/diagnostic?site=AS:3249";
 		
-		  String workspace = "";
-		  String as = "AS"; 
-
-		  //ToDo:
-		  //     1. to empty main method, create a separate method
-		  //     2. parametrize that method so that we can enter a string param
-		  //     3. do checks on param entry: whether FQDN or ASN (AS, no /$)
-		  //     4. some routine checks whether FQDN looks normalized (/ off)
+		  System.out.println("============= 1 - FQDN =============");
+		  grabClientReputation("www.ee");
+		  System.out.println("============= 2 - ASN =============");
+		  grabClientReputation("3249");
+		  
+		  // ToDo:
+		  //     DONE 1. to empty main method, create a separate method
+		  //     DONE 2. parametrize that method so that we can enter a string param
+		  //     DONE 3. do checks on param entry: whether FQDN or ASN (AS, no 
+		  //     4. some routine checks whether FQDN looks normalized (/$ off)
 		  //     5. separate AS info from AS number for future bookkeeping
+		  //	 6. compare uniqueness against the DB. Currently there _are_ repeats. 
 
-	try {
-	  String url = url1;
-	  System.out.println("START: " + url);
-	  Document doc = Jsoup.connect(url).get();
-	  Elements meaningfulSections = doc.select("a");
-	 // need to find memes that contain meaningful FQDNs.
-	  for(Element fqdnCandidate : meaningfulSections) {
-		 
-		 workspace = fqdnCandidate.text();
-	     // System.out.println(workspace);
-
-	  	 // Checking each meme for nonsenselessness   
-	  	    if ( ! (isNonsense(workspace))) { // throw away Google's own promo links
-		  	    if (workspace.contains(as)) {
-			 		System.out.println("Also found AS info: " + workspace);
-				// ToDo: the routine to exfiltrate all other ASNs except our initial one
-			 		// Is this ASN our initial ASN?
-		  	    } else {
-	  	    	System.out.println("Sub says SHIT FOUND AT: " + workspace);
-	  	    	// ToDo: We should extrafiltrate these to some global list
-
-	  	    } 
-	  	 }
-	  }  
-
-
-	} //TRY
-	
-	// Funny thing obtained from a source. Should we use it elsewhere?
-	// SRC: http://tutorials.jenkov.com/java-logging/logger.html
-	catch (IOException ex) {
-	  Logger.getLogger(parseGSBReputation.class.getName())
-	        .log(Level.SEVERE, null, ex);
-	   }
-	
+		
     } // MAIN
-} //CLASS
-	
 
+	} //CLASS
+	
