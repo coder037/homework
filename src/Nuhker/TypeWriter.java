a79e3d004cb0cb08b47c1797bf445b086083cce3
Coder 037 <coder@xyz.ee>
2014-12-09 15:59:33 +0200

    Class created to append results to a file

---
+++ src/Nuhker/TypeWriter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Nuhker;
+
+/**
+ * @author n0ne
+ *
+ */
+public class TypeWriter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String fileName, String toWrite) {
+	
+		System.out.println("===-> Writing another line to " + fileName);
+		
+		// try-catch
+		System.out.println("        succesfully.");
+		return;
+	}
+
+}
