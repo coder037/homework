57ff0f608f1676258638d8276353d1839b423daf
coder037 <coder037@xyz.ee>
2014-12-11 09:52:02 +0200

    a new global DataBase class created

---
+++ src/nuhker/DataBase.java
@@ -0,0 +1,30 @@
+/**
+ * created: Dec 11, 2014 9:48:34 AM
+ */
+package nuhker;
+
+import java.util.ArrayList;
+
+/**
+ * @author coder037@xyz.ee
+ * @identity 0fa1557ce3cbb37c25a6dd68a1f65c59d354b24788c39abf15fc2d1440d4f45c2f77425c1fe3d4b255fcd936042ef7ea0c202edbdd1505937da13455085c47ff
+ *
+ */
+public class DataBase {
+
+	// ArrayList<String> knownSites = new ArrayList<String>();
+
+	
+	
+	/**
+	 * This is the Global Database of targets.
+	 * Once here, we will not spend the time to ask site reputation twice
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	 
+		// much likely nothing here except test code
+		
+	}
+
+}
