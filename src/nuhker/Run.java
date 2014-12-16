93f9e772278d03f01ab3670338f97f137f895f2f
coder037 <coder037@xyz.ee>
2014-12-16 21:04:57 +0200

    mvn install working after pom.xml definitions corrected

--- src/nuhker/Run.java
+++ src/nuhker/Run.java
@@ -282,13 +282,13 @@ public class Run {
 		LOG.config("   0--------={Start}=--------0");
 		
 		// Alternatives for simulation (until we build the static CLI program)
-		String[] simulation1 = { "--country", "EE", "--copyright", "Some Name",
-				"--xtra", "-o", "output", "-R", "15", "-t", "2500", "-d", "FINEST",
-				"-M", "43200" };
-		String[] simulation2 = { "-c", "EE", "-d", "5", "-M", "80000", "-n",
-				"-o", "somefilename-001", "-R", "6", "-t", "2800" };
-		String[] simulation3 = { "--help" };
-		String[] effectiveOptions = simulation1; // vs argv
+//		String[] simulation1 = { "--country", "EE", "--copyright", "Some Name",
+//				"--xtra", "-o", "output", "-R", "15", "-t", "2500", "-d", "FINEST",
+//				"-M", "43200" };
+//		String[] simulation2 = { "-c", "EE", "-d", "5", "-M", "80000", "-n",
+//				"-o", "somefilename-001", "-R", "6", "-t", "2800" };
+//		String[] simulation3 = { "--help" };
+		String[] effectiveOptions = argv;
 
 	
 		LOG.severe("============= Starting M A I N ==============");
